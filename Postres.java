4089359aa93c6594cb99e166925d3aa169f69c30
Felix Reyes <freyesmendo8949@stu.neisd.net>
2016-02-18 12:36:04 -0600

    Felix

---
+++ Postres.java
@@ -0,0 +1,10 @@
+public class Postres{
+	public static void main(String[] args){
+		String[] DogShow = {"pooker doodle","great dane","Beagle","Boxer","Foxx runner","coon hound"};
+
+		System.out.println(DogShow[2] + DogShow[4]);
+		for(int i = 0; i< DogShow.length -1; i++){
+				System.out.println(DogShow[i]);
+		}
+	}
+}
